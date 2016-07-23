3a900294a36fe7014c6dcf8445bdea1557d2f704
richard <lichard49@gmail.com>
2016-07-23 02:22:19 -0400

    Simple implementation of adding waveforms

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,25 +5,40 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import javafx.util.Pair;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     private final Duration STEP_DURATION = Duration.millis(1000);
 
+    @FXML private BorderPane rootPane;
     @FXML private BorderPane mediaViewContainer;
     @FXML private MediaView mediaView;
     @FXML private Text videoTime;
+    @FXML private VBox waveformList;
 
     private MediaPlayer mediaPlayer;
 
@@ -73,5 +88,19 @@ public class Controller implements Initializable {
         videoTime.setText(String.format("%1$.3f / %2$.3f",
                 mediaPlayer.getCurrentTime().toSeconds(),
                 mediaPlayer.getTotalDuration().toSeconds()));
+    }
+
+    private void insertWaveform(List<XYChart.Data<Float, Float>> coordinates) {
+        LineChart<Number, Number> waveform = new LineChart<>(new NumberAxis(), new NumberAxis());
+        XYChart.Series series = new XYChart.Series();
+
+        for(XYChart.Data<Float, Float> coordinate : coordinates) {
+            series.getData().add(coordinate);
+        }
+
+        waveform.setLegendVisible(false);
+        waveform.getData().add(series);
+        waveform.prefWidthProperty().bind(rootPane.widthProperty());
+        waveformList.getChildren().add(waveform);
     }
 }
